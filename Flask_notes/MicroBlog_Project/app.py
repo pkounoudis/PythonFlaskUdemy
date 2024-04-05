58b3fa7bc72294c770eade809b2592d99223d65d
Panos Kounoudis <39673318+pkounoudis@users.noreply.github.com>
2024-04-05 19:35:23 +0300

    Add files via upload

---
+++ Flask_notes/MicroBlog_Project/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+import datetime
+
+app = Flask(__name__)
+
+entries = []
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        entry_content = request.form.get("content")
+        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+        entries.append((entry_content, formatted_date))
+
+    return render_template("home.html", entries = entries)
+
