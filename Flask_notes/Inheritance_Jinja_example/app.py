e11cc4b31533bf8763c08ca533acfce6cf0bd3b5
Panos Kounoudis <39673318+pkounoudis@users.noreply.github.com>
2024-05-15 21:16:52 +0300

    Update app.py

--- Flask_notes/Inheritance_Jinja_example/app.py
+++ Flask_notes/Inheritance_Jinja_example/app.py
@@ -1,1 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+todos = [("Get milk", False), ("Learn programming", True)]
+
+@app.route("/")
+def todo():
+    return render_template("Jinja_mastery_I.html", todos = todos)
 
