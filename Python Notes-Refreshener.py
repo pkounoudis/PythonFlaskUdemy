dd84dcbe8875e73d3971444e5de7153a6a0422ea
Panos Kounoudis <39673318+pkounoudis@users.noreply.github.com>
2024-06-18 22:58:18 +0300

    Update Python Notes-Refreshener.py

--- Python Notes-Refreshener.py
+++ Python Notes-Refreshener.py
@@ -78,7 +78,7 @@ class Device:
 
         self.name = name
         self.connected_by = connected_by
-        self.connected = True
+        self.connected = False
 
     def __str__(self):
         return f"Device {self.name!r} ({self.connected_by})"
